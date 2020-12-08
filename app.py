d81512c8d88c0fa735a8046d44a089e1898400f7
Sohaibstylo <sohaibstylo03332140546@gmail.com>
2020-12-07 19:16:48 -0800

    updates

--- app.py
+++ app.py
@@ -2,17 +2,15 @@ from flask import Flask , render_template ,request ,flash , redirect , url_for
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-secret=secretKey.s_key()
-app.config["SECRET_KEY"] = secret
 
 app.config['MONGO_URI']="mongodb+srv://Sohaib:sohaib12439@cluster0-5ij3o.gcp.mongodb.net/AutonomousServingSystem?retryWrites=true&w=majority"
 mongo=PyMongo(app)
 
-@app.route("/lift")
+@app.route("/")
 def lift():
     cur=mongo.db.Lift
     data=cur.find_one({"Title":"Lift"})
-    return render_template("Lift.html",data=data)
+    return render_template("elevator.html",data=data)
 
 if __name__=='__main__':
     app.run(debug = True,port=1267)
